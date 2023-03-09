21a2fb8ede67a9cb401ea2732738398351d3f8f6
renato.mello <renato.mello@globalhitss.com.br>
2023-03-09 15:54:49 -0300

    tapa no visual e melhoria na logica de exclusão

--- lista-compras-script.js
+++ lista-compras-script.js
@@ -40,7 +40,7 @@ addButton.addEventListener("click", () => {
 
 			// Cria categoria caso não exista e adiciona o item
 			const notInArray = listaCompras.every((item) => item.categoria !== listItem[0].categoria)
-			//console.log(notInArray)
+
 			if (notInArray) {
 				listaCompras.push(...listItem)
 			}
@@ -51,7 +51,6 @@ addButton.addEventListener("click", () => {
 })
 
 finishButton.addEventListener("click", () => {
-	deleteButton.removeAttribute("disabled")
 	let ulCollection = document.getElementsByTagName("ul")
 	let ulArray = Array.from(ulCollection)
 
@@ -78,22 +77,49 @@ function renderList() {
 	uniqueCategories.map((category) => {
 		let itemUl = document.createElement("ul")
 		itemUl.id = category
+		itemUl.classList.add("bg-amber-200", "rounded", "flex", "flex-col", "self-start", "shadow-lg", "shadow-amber-500/50")
 
 		let span = document.createElement("span")
+		span.classList.add("text-xl", "font-bold", "capitalize", "p-4", "text-center", "bg-amber-400", "text-amber-900", "rounded-t")
 		span.innerText = category
 		itemUl.appendChild(span)
 
 		// Distribui os itens em suas respectivas categorias
-		listaCompras.map((item) => {
+		listaCompras.map((item, itemIndex) => {
 			if (item.categoria == category) {
 				item.item.map((i) => {
 					let itemLi = document.createElement("li")
+					itemLi.classList.add("flex", "items-center", "px-4", "py-2", "justify-between", "text-lg", "border-b", "border-amber-500", "last:rounded-b")
 					itemLi.innerText = i
 					let deleteButton = document.createElement("button")
 					deleteButton.addEventListener("click", () => {
 						itemLi.remove()
+						let itemItemIndex = item.item.indexOf(i)
+
+						item.item.splice(itemItemIndex, 1)
+
+						if (item.item.length == 0) {
+							listaCompras.splice(itemIndex, 1)
+							checkListContent(category)
+						}
 					})
-					deleteButton.innerText = "x"
+					deleteButton.innerHTML = '<img class="w-6 text-white" src="./images/trash.svg">'
+					deleteButton.classList.add(
+						"inline-flex",
+						"justify-center",
+						"rounded-md",
+						"bg-red-600",
+						"py-2",
+						"px-3",
+						"font-semibold",
+						"text-white",
+						"shadow-sm",
+						"hover:bg-red-500",
+						"focus-visible:outline",
+						"focus-visible:outline-2",
+						"focus-visible:outline-offset-2",
+						"focus-visible:outline-red-500"
+					)
 					itemLi.appendChild(deleteButton)
 					itemUl.appendChild(itemLi)
 				})
@@ -102,4 +128,12 @@ function renderList() {
 			}
 		})
 	})
+}
+
+function checkListContent(category) {
+	let lista = document.getElementById(category)
+
+	if (lista.childElementCount <= 1) {
+		lista.remove()
+	}
 }
